55065e4232f79cffeeb68a49359bd3e6675342c1
Subhash Ramesh <subby8pie@gmail.com>
2017-08-10 07:21:11 -0700

    Update __init__.py
    Commented absolute_import line

--- kapre/__init__.py
+++ kapre/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+# This library only works on Python 2.7 if this line is commented: from __future__ import absolute_import
 
 __version__ = '0.1.2'
 VERSION = __version__
